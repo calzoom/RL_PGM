8e1e5063fa10ef557002f05759f5368eb877dee3
Japjot Singh <33074314+calzoom@users.noreply.github.com>
2021-12-16 05:28:42 -0800

    cleaned up evals

--- run_script.sh
+++ run_script.sh
@@ -24,4 +24,14 @@
 export PYTHONUNBUFFERED=1
 conda activate maze_smaac
 cd fp
-python AC_JOINT/test.py -n=joint_wcci_run_ap200 --controller=./result/wcci_run_0/model/ --c_suffix=last -s=0 -c=wcci -ap=200
+# Evaluate
+srun -N 1 -n 1 python AC_JOINT/evaluate.py -data="/data/rkunani/smaac_data" --ap=50 --c_suffix=100000 -n=joint_wcci-run-0_GAT-k20_100k_0
+
+# srun -N 1 -n 1 --gres=gpu:1 python AC_JOINT/evaluate.py \
+# --ap=50 \
+# --c_suffix=20000 \
+# -n=joint_wcci-run-0_GAT-k200_100k_requeue_0
+
+# srun -N 1 -n 1 --gres=gpu:1 python AC_JOINT/evaluate.py -n=joint_wcci_run_0
+# srun -N 1 -n 1 --gres=gpu:1 python AC_JOINT/evaluate.py -n=joint_wcci_run_ap20_0
+# srun -N 1 -n 1 --gres=gpu:1 python AC_JOINT/evaluate.py -n=joint_wcci_run_ap200_0
